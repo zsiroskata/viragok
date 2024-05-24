fc74ef3cae7191f2738787ebc25e68e80a1b80da
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2024-05-24 07:59:31 +0200

    Add project files.

---
+++ viragokZsK/Flower.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace viragokZsK
+{
+	internal class Flower
+	{
+		public string Name { get; set; }
+        public int Price { get; set; }
+        public string Color { get; set; }
+		public string Img { get; set; }
+
+		public Flower(string sorok) 
+		{
+			var sor = sorok.Split(";");
+			Name = sor[0];
+			Price = int.Parse(sor[1]);
+			Color = sor[2];
+			Img = sor[3];
+		}
+    }
+}
---
+++ viragokZsK/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace viragokZsK
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		List<Flower> flowers = new();
+		List<Flower> name = new();
+		
+		public MainWindow()
+		{
+			InitializeComponent();
+			using StreamReader sr = new StreamReader(
+				path: @"..\..\..\src\flowers.txt",
+				encoding: Encoding.UTF8
+				);
+			while ( !sr.EndOfStream ) 
+			{
+                flowers.Add(new Flower(sr.ReadLine()));
+			}
+		
+		//flowers.OrderBy(flower => flower.Name.Reverse());
+
+
+			sr.Close();
+			if (flowers != null)
+			{
+				foreach (var item in flowers)
+				{
+					
+					FlowerList.Items.Add(item.Name);
+
+					PriceList.Items.Add(item.Price);
+					ColorList.Items.Add(item.Color);
+				}				
+			}	
+
+		}
+
+		private void deletButton_Click(object sender, RoutedEventArgs e)
+		{
+            if (FlowerList.SelectedItem != null || ColorList.SelectedItem != null || PriceList.SelectedItem != null)
+			{
+				int index = FlowerList.SelectedIndex;
+				FlowerList.Items.Remove(FlowerList.SelectedItem);
+				ColorList.Items.Remove(ColorList.SelectedItem);
+				PriceList.Items.Remove(PriceList.SelectedItem);
+			}
+            else
+            {
+				MessageBox.Show("nem jelöltél ki semmit");
+            }
+
+        }
+
+		private void copyButton_Click(object sender, RoutedEventArgs e)
+		{
+            if (FlowerList.SelectedItem != null)
+            {
+				CopyList.Items.Add(FlowerList.SelectedItem); 
+            }
+            else
+            {
+				MessageBox.Show("nem jelöltél ki semmit a virágok közül");
+            }
+        }
+
+	
+	}
+}
